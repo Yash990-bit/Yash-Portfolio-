113cecaf1112ca2290493bc98838e755f9476fcf
Yash Raghubanshi <yash.raghubanshi2024@nst.rishihood.edu.in>
2026-06-22 08:13:14 +0530

    change role

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -14,7 +14,7 @@ const HomePage = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [typingSpeed, setTypingSpeed] = useState(150);
 
-    const roles = ["AI/ML Engineer", "Machine Learning Engineer", "Full Stack Developer"];
+    const roles = ["AI/ML Engineer", "Data Engineer", "Full Stack Developer"];
 
     useEffect(() => {
         const handleTyping = () => {
